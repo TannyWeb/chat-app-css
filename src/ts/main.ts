1ee16538faf054ad9278295a82e173ff438ed01b
tanvir <tanvir.ali@iprospect.com>
2021-01-17 06:50:13 +0000

    added basic timeline animation

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,11 +5,11 @@ import { gsap } from "gsap";
 const GoogleImages = require('google-images');
 const client = new GoogleImages('342b958892350d62b', 'AIzaSyAdHajPUMNAltSNdUh5l_Lyym8QJREtbkE');
 
-const btn = document.querySelector('#send');
+const btn = document.querySelector('#send') as HTMLElement;
 const recieveBtn = document.querySelector('#recieve') as HTMLElement;
 const dateBtn = document.querySelector('#screenDate') as HTMLElement;
 
-const fomattedDate = new Date().toDateString();
+const fomattedDate: string = new Date().toDateString();
 dateBtn.innerText = fomattedDate;
 
 btn.addEventListener('click', (e: Event) => {
@@ -17,7 +17,10 @@ btn.addEventListener('click', (e: Event) => {
     sendMessage()
 });
 
-gsap.fromTo('#bg-design', { y: -800, duration: 1 }, { y: 0 })
+let tl = gsap.timeline();
+tl.fromTo('#bg-design', { y: -800, duration: 1 }, { y: 0 })
+    .fromTo('#phone', { visibility: "hidden" }, { visibility: "visible" })
+    .fromTo('#screenDate', { display: "none" }, { display: "block" })
 
 recieveBtn.addEventListener('click', (e: Event) => {
     e.preventDefault();
